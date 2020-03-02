54b60716667f1ebe2a1027a63fe951a2f016332c
Soft-Bullet <binaytiwari6@gmail.com>
2020-03-02 04:05:43 +0000

    a5ultexx: Move all blobs to vendor

--- Android.mk
+++ Android.mk
@@ -74,11 +74,12 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := vendor.qti.hardware.fm@1.0
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/lib/vendor.qti.hardware.fm@1.0.so
+LOCAL_SRC_FILES := proprietary/vendor/lib/vendor.qti.hardware.fm@1.0.so
 LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
